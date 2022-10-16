c73f8a417d5f79456ee19d9c84603b2be6ff0752
Felipe V. Rigo <github@felipevr.eti.br>
2022-10-15 20:01:23 -0400

    Cap. 2

--- jogo.py
+++ jogo.py
@@ -12,12 +12,18 @@ chute = int(chute_str)
 
 #print("Você digitou: ", chute)
 
-print(type(numero_secreto))
-print(type(chute))
+#print(type(numero_secreto))
+#print(type(chute))
 
-if (numero_secreto == chute):
-    print('Você acertou!')
-else:
-    print("Você errou!")
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
 
-print("Fim do jogo")
+if (acertou):
+    print("Você acertou!")
+elif (maior):
+    print("Você errou! O seu chute foi maior que o número secreto.")
+elif (menor):
+    print("Você errou! O seu chute foi menor que o número secreto.")
+
+#print("Fim do jogo")
