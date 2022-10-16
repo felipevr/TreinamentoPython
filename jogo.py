94950d00701bb525c5f641a55477fc50b7ab16c4
Felipe V. Rigo <github@felipevr.eti.br>
2022-10-15 20:08:33 -0400

    melhorias Cap. 4

--- jogo.py
+++ jogo.py
@@ -11,8 +11,7 @@ total_de_tentativas = 3
 rodada = 1
 
 while (rodada <= total_de_tentativas):
-    print("Tentativa", rodada, "de", total_de_tentativas)
-
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
     chute_str = input("Digite o seu número: ")
 
@@ -24,6 +23,7 @@ while (rodada <= total_de_tentativas):
 
     if (acertou):
         print("Você acertou!")
+        break
     elif (maior):
         print("Você errou! O seu chute foi maior que o número secreto.")
     elif (menor):
