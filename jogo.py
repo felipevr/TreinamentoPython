9dc92e507e43659e9f05b63b432618290ad8af2a
Felipe V. Rigo <github@felipevr.eti.br>
2022-10-15 21:16:39 -0400

    Cap. 5

--- jogo.py
+++ jogo.py
@@ -8,14 +8,16 @@ numero_secreto = 42
 
 total_de_tentativas = 3
 
-rodada = 1
-
-while (rodada <= total_de_tentativas):
+for rodada in range(1,total_de_tentativas+1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
 
     chute = int(chute_str)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
