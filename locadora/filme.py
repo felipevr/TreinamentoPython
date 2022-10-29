b4bc10707e1335309cf532e4badfa85817bea307
Felipe V. Rigo <github@felipevr.eti.br>
2022-10-29 18:38:06 -0400

    Ajuste de herança (Classe base)

--- locadora/filme.py
+++ locadora/filme.py
@@ -1,22 +1,6 @@
-
-class Filme:
+from artigo import Artigo
+class Filme (Artigo):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.__likes = 0
 
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
--- locadora/serie.py
+++ locadora/serie.py
@@ -1,22 +1,7 @@
+from artigo import Artigo
 
-class Serie:
+class Serie (Artigo):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes += 1
+        
