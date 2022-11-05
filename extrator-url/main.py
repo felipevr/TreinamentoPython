9eaa2c2680ea214397c3952f1a8d87ec11b9e19b
Felipe V. Rigo <github@felipevr.eti.br>
2022-11-05 19:59:30 -0400

    pega o valor de um parametro

--- extrator-url/main.py
+++ extrator-url/main.py
@@ -1,12 +1,22 @@
 url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 
-url = "bytebank.com/cambio?moedaOrigem=real"
+#url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
 #print(url)
 
+#Separa base e os parametros
 indice_interrogacao = url.find('?')
-url_base = url[0:indice_interrogacao]
-print(url_base)  # Vai imprimir “bytebank.com/cambio
-
-
+url_base = url[:indice_interrogacao]
 url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+# Busca o valor de um parâmatro
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_separador = url_parametros.find('&', indice_valor)
+if (indice_separador == -1):
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_separador]
+
+print(valor)
