018b671dcc58188e4203d6d5597edee239f8efa4
Felipe V. Rigo <github@felipevr.eti.br>
2022-11-23 21:24:07 -0400

    brincando com dicionarios (array associativo)

---
+++ agenda/index.py
@@ -0,0 +1,14 @@
+
+telefones = ['1234-5678', '9999-9999', '8765-4321', '8877-7788']
+
+
+contato = ('Yan', '1234-5678')
+
+
+contatos_lista = [('Yan', '1234-5678'), ('Pedro', '9999-9999'),
+                    ('Ana', '8765-4321'), ('Marina', '8877-7788')]
+
+print(contatos_lista[3][1])
+
+contatos = {'Yan': '1234-5678'}
+print(type(contatos))
