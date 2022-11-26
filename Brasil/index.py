0ca953baff1b57286ca6b4d45394b6abc64e2da7
Felipe V. Rigo <github@felipevr.eti.br>
2022-11-26 08:13:15 -0400

    Validação CPF (WIP)

--- Brasil/index.py
+++ Brasil/index.py
@@ -2,7 +2,18 @@
 # Curso Python Brasil: validação de dados nacional
 # https://cursos.alura.com.br/course/python-validacao-dados/task/62231
 
-cpf = 15616987913
-cpf = str(cpf)
-tamanho_cpf = len(cpf)
-print(tamanho_cpf)
+from Cpf import Cpf
+
+cpf = '15616987913'
+
+objeto_cpf = Cpf(cpf)
+
+print(objeto_cpf)
+
+
+fatia_um = cpf[:3]
+fatia_dois = cpf[3:6]
+fatia_tres = cpf[6:9]
+fatia_quatro = cpf[9:]
+
+print(fatia_um, fatia_dois, fatia_tres, fatia_quatro)
