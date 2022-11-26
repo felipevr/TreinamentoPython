1689ebcf1fa10ee983b55e811fcda14178bd2e45
Felipe V. Rigo <github@felipevr.eti.br>
2022-11-26 15:43:44 -0400

    formata CPF

--- Brasil/index.py
+++ Brasil/index.py
@@ -16,4 +16,8 @@ fatia_dois = cpf[3:6]
 fatia_tres = cpf[6:9]
 fatia_quatro = cpf[9:]
 
-print(fatia_um, fatia_dois, fatia_tres, fatia_quatro)
+cpf_formatado = "{}.{}.{}-{}".format(
+    fatia_um, fatia_dois, fatia_tres, fatia_quatro
+)
+
+print(cpf_formatado)
