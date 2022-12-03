2d3b4923578aab8f55b401fb96831db46db727b1
Felipe V. Rigo <github@felipevr.eti.br>
2022-12-03 08:30:20 -0400

    mais testes

--- bytebank/main.py
+++ bytebank/main.py
@@ -8,4 +8,10 @@ def teste_idade():
     funcionario_teste = Funcionario('Teste', '13/03/2000', 1111)
     print('Teste = {}'.format(funcionario_teste.idade()))
     
+    funcionario_teste1 = Funcionario('Teste', '13/03/1999', 1111)
+    print('Teste = {}'.format(funcionario_teste1.idade()))
+    
+    funcionario_teste1 = Funcionario('Teste', '11/08/1999', 1111)
+    print('Teste = {}'.format(funcionario_teste1.idade()))
+    
 teste_idade()
